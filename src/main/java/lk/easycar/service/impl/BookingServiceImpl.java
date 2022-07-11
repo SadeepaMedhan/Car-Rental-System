6ea7f480653a95dbd24396da817c5ea498a62c0c
SadeepaMedhan <sadeepamedhan823@gmail.com>
2022-07-12 00:36:29 +0530

    update

--- src/main/java/lk/easycar/service/impl/BookingServiceImpl.java
+++ src/main/java/lk/easycar/service/impl/BookingServiceImpl.java
@@ -2,7 +2,11 @@ package lk.easycar.service.impl;
 
 import lk.easycar.dto.BookingDTO;
 import lk.easycar.entity.Booking;
+import lk.easycar.entity.Driver;
+import lk.easycar.entity.Vehicle;
 import lk.easycar.repo.BookingRepo;
+import lk.easycar.repo.DriverRepo;
+import lk.easycar.repo.VehicleRepo;
 import lk.easycar.service.BookingService;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
@@ -17,15 +21,35 @@ import java.util.List;
 public class BookingServiceImpl implements BookingService {
 
     @Autowired
-    private BookingRepo repo;
+    private BookingRepo bookingRepo;
+
+    @Autowired
+    private VehicleRepo vehicleRepo;
+
+    @Autowired
+    private DriverRepo driverRepo;
 
     @Autowired
     private ModelMapper mapper;
 
     @Override
     public void saveBooking(BookingDTO dto) {
-        if (!repo.existsById(dto.getBookingId())) {
-            repo.save(mapper.map(dto, Booking.class));
+        if (!bookingRepo.existsById(dto.getBookingId())) {
+            bookingRepo.save(mapper.map(dto, Booking.class));
+
+            if (dto.getVehicleId() == null) {
+                throw new RuntimeException("No vehicle added the booking");
+            }else {
+                Vehicle vehicle = vehicleRepo.findById(dto.getVehicleId()).get();
+                vehicle.setStatus("Selected");
+                vehicleRepo.save(vehicle);
+
+                if (dto.getDriverId() != null){
+                    Driver driver = driverRepo.findById(dto.getDriverId()).get();
+
+                }
+            }
+
         } else {
             throw new RuntimeException("Booking Already Exist..!");
         }
@@ -48,7 +72,7 @@ public class BookingServiceImpl implements BookingService {
 
     @Override
     public List<BookingDTO> getAllBookings() {
-        return mapper.map(repo.findAll(), new TypeToken<List<BookingDTO>>() {
+        return mapper.map(bookingRepo.findAll(), new TypeToken<List<BookingDTO>>() {
         }.getType());
     }
 }
