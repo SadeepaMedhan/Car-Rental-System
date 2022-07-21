e5c4ed5ec90ce429037d5fe820c0a8695204ed73
SadeepaMedhan <sadeepamedhan823@gmail.com>
2022-07-22 01:02:17 +0530

    update

--- frontend/src/pages/Dashboard/index.jsx
+++ frontend/src/pages/Dashboard/index.jsx
@@ -24,10 +24,42 @@ import DirectionsCarIcon from '@mui/icons-material/DirectionsCar';
 import PeopleIcon from '@mui/icons-material/People';
 import SwitchAccountIcon from '@mui/icons-material/SwitchAccount';
 import LeaderboardIcon from '@mui/icons-material/Leaderboard';
+import EventIcon from '@mui/icons-material/Event';
 import backImg2 from "../../assets/images/dashBack3.webp";
 import {Stack} from "@mui/material";
 import SmallVehicleCard from "../../components/Card/smallVehicleCard";
 import ImageIcon from '@mui/icons-material/Image';
+import { DataGrid } from '@mui/x-data-grid';
+import {styleSheet} from "./style";
+
+function SummaryCard(props) {
+    let classes = styleSheet();
+    return(
+        <div>
+            <Stack className={classes.card_box}>
+                <div className={classes.card_logo_box}>
+                    {props.icon}
+                </div>
+                <Stack direction="column"
+                       justifyContent="flex-start"
+                       alignItems="flex-end"
+                       spacing={1}
+                       style={{paddingRight:'5px',width: '100%', height:'100%'}}>
+                    <h3>{props.tittle}</h3>
+                    <h2>{props.tCount}</h2>
+
+                </Stack>
+                <Divider />
+                <Stack direction="row"
+                       justifyContent="flex-start" spacing={1}
+                       alignItems="center" style={{width: '100%', height:'40px'}}>
+                    {props.label}
+                </Stack>
+            </Stack>
+        </div>
+    )
+
+}
 
 const btnArray1 = [
     { name:'Dashboard', icon: <DashboardIcon />},
@@ -113,6 +145,7 @@ const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' 
 );
 
 export default function Dashboard() {
+    let classes = styleSheet();
     const theme = useTheme();
     const [open, setOpen] = React.useState(false);
 
@@ -217,17 +250,83 @@ export default function Dashboard() {
             </Drawer>
             <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
                 <DrawerHeader />
-                <Stack direction={{ xs: 'column', sm: 'row' }}
-                       spacing={{ xs: 1, sm: 2, md: 4 }}
-                       justifyContent="center"
-                       alignItems="center"
-                       mt={2} >
+                <Stack spacing={2}>
+                    <Stack direction={{ xs: 'column', sm: 'row' }}
+                           spacing={{ xs: 1, sm: 2, md: 4 }}
+                           justifyContent="center"
+                           alignItems="center"
+                           mt={2} >
 
-                    <SmallVehicleCard imgSrc={reactIcon} name="50 Customers" description="best review"/>
+                        <SummaryCard icon={<CollectionsBookmarkIcon style={{fontSize: '34px'}} className={classes.card_logo} />}
+                        tittle="Bookings" tCount="550" label={<EventIcon/>}/>
 
 
+
+                    </Stack>
+                    <Stack>
+                        <Table/>
+                    </Stack>
                 </Stack>
+
             </Box>
+        </Box>
+    );
+}
+
+const columns = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    {
+        field: 'firstName',
+        headerName: 'First name',
+        width: 150,
+        editable: false,
+    },
+    {
+        field: 'lastName',
+        headerName: 'Last name',
+        width: 150,
+        editable: true,
+    },
+    {
+        field: 'age',
+        headerName: 'Age',
+        type: 'number',
+        width: 110,
+        editable: true,
+    },
+    {
+        field: 'fullName',
+        headerName: 'Full name',
+        description: 'This column has a value getter and is not sortable.',
+        sortable: false,
+        width: 160,
+        valueGetter: (params) =>
+            `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+    },
+];
+
+const rows = [
+    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+];
+function Table() {
+    return (
+        <Box sx={{ height: 400, width: '100%' }}>
+            <DataGrid
+                rows={rows}
+                columns={columns}
+                pageSize={5}
+                rowsPerPageOptions={[5]}
+                checkboxSelection
+                disableSelectionOnClick
+            />
         </Box>
     );
 }
---
+++ frontend/src/pages/Dashboard/style.js
@@ -0,0 +1,29 @@
+import {createUseStyles} from 'react-jss'
+
+
+export const styleSheet = createUseStyles({
+    card_box:{
+        width: '200px',
+        height:'120px',
+        position:'relative',
+        border:'1px solid #BDBDBD',
+        padding:'5px',
+        borderRadius:'5px',
+        boxShadow: ' rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px'
+    },
+    card_logo_box:{
+        width: '70px',
+        height:'70px',
+        position:'absolute',
+        top:'-16px', left:'18px',
+        backgroundColor:'green',
+        borderRadius:'5px',
+        boxShadow: 'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px'
+    },
+    card_logo:{
+        right: 0, left: 0, top: 0, bottom: 0,
+        margin: 'auto', position: 'absolute',
+         color:'white'
+    }
+
+})
